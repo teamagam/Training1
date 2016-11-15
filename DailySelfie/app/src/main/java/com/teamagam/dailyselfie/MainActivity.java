dc8fab2ec5442ce27c8098bc00e02fbb583f6e2a
Yoni <teamagam1@gmail.com>
2016-11-15 15:39:11 +0200

    Added new method to load pictures info from the dedicated directory

--- DailySelfie/app/src/main/java/com/teamagam/dailyselfie/MainActivity.java
+++ DailySelfie/app/src/main/java/com/teamagam/dailyselfie/MainActivity.java
@@ -1,11 +1,14 @@
 package com.teamagam.dailyselfie;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +23,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRecyclerView() {
-        mPictureAdapter = new PictureAdapter(this, new ArrayList<PictureInfo>());
+        mPictureAdapter = new PictureAdapter(this, loadPictureInfoList());
         mRecyclerView = (RecyclerView) findViewById(R.id.pictures_recycler);
         mRecyclerView.setAdapter(mPictureAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private List<PictureInfo> loadPictureInfoList() {
+        List<PictureInfo> pictureInfoList = Collections.emptyList();
+        File externalFilesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (null != externalFilesDir) {
+            File[] files = externalFilesDir.listFiles();
+            if (null != files) {
+                for (int i = 0; i < files.length; i++) {
+                    PictureInfo pictureInfo = new PictureInfo();
+                    pictureInfo.mPath = files[i].getPath();
+                    pictureInfo.mFileName = files[i].getName();
+                    pictureInfoList.add(pictureInfo);
+                }
+            }
+        }
+        return pictureInfoList;
     }
 
 }
