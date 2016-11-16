66e63b29a031eb0ac30d2a4680b9df9f4b746c8e
Ido <teamagam5@gmail.com>
2016-11-16 11:05:59 +0200

    Fixed after CR

--- DailySelfie/app/src/main/java/com/teamagam/dailyselfie/MainActivity.java
+++ DailySelfie/app/src/main/java/com/teamagam/dailyselfie/MainActivity.java
@@ -6,20 +6,21 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private PictureAdapter mPictureAdapter;
     private RecyclerView mRecyclerView;
-    private List<PictureInfo> mPictureInfoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPictureInfoList = new ArrayList<>();
-        mPictureAdapter = new PictureAdapter(this, mPictureInfoList);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        mPictureAdapter = new PictureAdapter(this, new ArrayList<PictureInfo>());
         mRecyclerView = (RecyclerView) findViewById(R.id.pictures_recycler);
         mRecyclerView.setAdapter(mPictureAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
