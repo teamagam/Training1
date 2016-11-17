6e75d924f98c110c59801cfacaba65300171e59d
Ido <teamagam5@gmail.com>
2016-11-17 14:42:43 +0200

    CLeaned code

--- DailySelfie/app/src/main/java/com/teamagam/dailyselfie/LoadPictureTask.java
+++ DailySelfie/app/src/main/java/com/teamagam/dailyselfie/LoadPictureTask.java
@@ -1,49 +1,33 @@
 package com.teamagam.dailyselfie;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
 import java.lang.ref.WeakReference;
 
 class LoadPictureTask extends AsyncTask<String, Void, Bitmap> {
+    private final Context mContext;
     private final WeakReference<ImageView> mImageViewReference;
-    final static String TAG = "DailySelfie";
 
-    LoadPictureTask(ImageView imageView) {
+    LoadPictureTask(Context context, ImageView imageView) {
         // Use a WeakReference to ensure the ImageView can be garbage collected
+        mContext = context;
         mImageViewReference = new WeakReference<>(imageView);
     }
-
 
     @Override
     protected Bitmap doInBackground(String... params) {
         String path = params[0];
-
-
-/*        Log.i(MainActivity.TAG, "Entered doInBackground");
-        Bitmap bitmap;
-        try {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(new File(path)));
-            bitmap = BitmapFactory.decodeStream(bufferedInputStream);
-            if (null != bitmap) {
-                Log.i(MainActivity.TAG, "Bitmap loaded in doInBackground");
-            } else {
-                Log.i(MainActivity.TAG, "null loaded in doInBackground");
-            }
-        } catch (IOException e) {
-            Log.i(MainActivity.TAG, "IOException in doInBackground");
-            bitmap = null;
-            //TODO
-        }*/
-
-
-        return decodeSampledBitmapFromFile(path, 64, 64);
+        int thumbnailSize = mContext.getResources().getInteger(R.integer.thumbnail_size);
+        return decodeSampledBitmapFromFile(path, thumbnailSize, thumbnailSize);
     }
 
     @Override
-    protected void onPostExecute(Bitmap bitmap){
+    protected void onPostExecute(Bitmap bitmap) {
         if (mImageViewReference != null && bitmap != null) {
             final ImageView imageView = mImageViewReference.get();
             if (imageView != null) {
@@ -52,42 +36,40 @@ class LoadPictureTask extends AsyncTask<String, Void, Bitmap> {
         }
     }
 
-    private static Bitmap decodeSampledBitmapFromFile(String path,
-                                                      int reqWidth, int reqHeight) {
+    private static Bitmap decodeSampledBitmapFromFile(String path, int requiredWidth, int requiredHeight) {
 
         // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
+        final Options options = new Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
 
         // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inSampleSize = calculateSampleSize(options, requiredWidth, requiredHeight);
 
         // Decode bitmap with inSampleSize set
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeFile(path, options);
     }
 
-    private static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    private static int calculateSampleSize(Options options, int requiredWidth, int requiredHeight) {
         // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
-        int inSampleSize = 1;
+        int sampleSize = 1;
 
-        if (height > reqHeight || width > reqWidth) {
+        if (height > requiredHeight || width > requiredWidth) {
 
             final int halfHeight = height / 2;
             final int halfWidth = width / 2;
 
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // Calculate the largest sampleSize value that is a power of 2 and keeps both
             // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
+            while ((halfHeight / sampleSize) >= requiredHeight
+                    && (halfWidth / sampleSize) >= requiredWidth) {
+                sampleSize *= 2;
             }
         }
 
-        return inSampleSize;
+        return sampleSize;
     }
 }
